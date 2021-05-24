de94a1655bfde12a125a37073599e4c371a9dd52
Valeriia <valeriia.air@gmail.com>
2021-05-24 17:32:13 +0300

    Переключатель цветов
    js + оформление кнопок

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './sass/main.scss';
 
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+const body = document.querySelector('body');
+let intervalColorChange = null;
+
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -7,8 +12,42 @@ const colors = [
   '#FF9800',
   '#009688',
   '#795548',
+  '#99CCFF',
+  '#CC99CC',
+  '#CCCCCC',
+  '#FF99FF',
+  '#33CCCC',
+  '#008B8B',
+  '#E0FFFF',
+  '#FFDAB9',
+  '#6495ED',
+  '#FFFAFA',
+  '#FF00FF',
+  '#2F4F4F',
+  '#FFFF00',
+  '#FFFF99',
+  '#7FFFD4',
+  '#FF4500',
+  '#336666',
+  '#990000',
 ];
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const bodyColorChange = function () {
+  body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+};
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  intervalColorChange = setInterval(() => {
+    bodyColorChange();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.disabled = false;
+  clearInterval(intervalColorChange);
+});
